200babcc4299a2094d8a475690607bc45ae5e1c8
MarlaSantos <72895099+marlasantos24@users.noreply.github.com>
2022-03-06 11:07:44 -0500

    Añadiendo_Elementos

--- app/src/main/java/com/example/ejemplotoolbar/MainActivity.kt
+++ app/src/main/java/com/example/ejemplotoolbar/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.ejemplotoolbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    var toolbar:Toolbar? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar)
+        toolbar?.setTitle(R.string.app_name)
+        setSupportActionBar(toolbar)
     }
 }
